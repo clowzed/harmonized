d88837e77bdd91e70e1c8e8124cde8dad01a5ace
clowzed <clowzed.work@gmail.com>
2022-01-29 19:38:03 +0300

    added logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use log::{debug, error, info, warn};
+
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -10,9 +13,17 @@ use text_io::read;
 fn main() -> Result<(), fraction::error::ParseError>
 {
 
+    dotenv::dotenv().ok();
+    env_logger::init();
+    
     //? Config
     let accords_amount = 8;
     let tacts_volume = fraction::Fraction::new(1 as u64, 1 as u64);
+    
+    debug!("Accords amount: {}", accords_amount);
+    debug!("Tacts   volume: {}", tacts_volume);
+
+
 
     //? Temp variables
     let mut current_tact = tacts::Tact::new(tacts_volume);
@@ -24,14 +35,15 @@ fn main() -> Result<(), fraction::error::ParseError>
 
 
 
-    for _ in 0..accords_amount
+    for accord_index in 0..accords_amount
     {
-
-
-
+        debug!("filling accord with number: {}", accord_index);
+        
         //? checking if tact is full
         if !accords_durations_sum.is_nan() && accords_durations_sum >= tacts_volume
         {
+            debug!("Tact volume is full! Creating new tact.");
+            
             all_tacts.push(current_tact);
             current_tact = tacts::Tact::new(tacts_volume);
             accords_durations_sum = fraction::Fraction::new(0_u64, 0_u64);
@@ -48,15 +60,21 @@ fn main() -> Result<(), fraction::error::ParseError>
 
         println!("Note height - octave : ");
         let octave:u8 = read!();
+        
+        debug!("Entered octave: {}", octave);
 
         println!("Note height - number : ");
         let number: u8 = read!();
-        println!("{}", number);
+        debug!("Entered number: {}", number);
 
         let note_height = notes::NoteHeight::new(octave, number);
+        debug!("Created note height: {:?}", note_height);
+        
         
         println!("Note duration: ");
         let duration:String = read!();
+             
+    
         let duration = fraction::Fraction::from_decimal_str(&duration)?;
 
         let new_note = notes::Note::new(note_height, duration);
