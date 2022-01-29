9281a1842d8101490197d649aa0c43ec4d8c89e3
clowzed <clowzed.work@gmail.com>
2022-01-29 20:08:25 +0300

    Fixed octave check in note height and added tests for it

--- src/notes.rs
+++ src/notes.rs
@@ -35,8 +35,11 @@ impl NoteHeight
 {
     pub fn new(octave: u8, number: u8) -> NoteHeight
     {
+        let new_number = match number % 7 {0 => 7, _ => number % 7};
+        let new_octave = match number % 7 {0 => octave + number / 7 - 1, _ => octave + number / 7};
+        
         //? Checking if number is too big for octave
-        NoteHeight{octave: octave + number / 7, number: number + number % 7}
+        NoteHeight{octave: new_octave, number: new_number}
     }
 }
 
@@ -48,6 +51,7 @@ impl Note
         {
             return Note{height, duration}
         }
+        
         panic!("Incoorect duration of note! Got: {}. Available: {}", 
                 duration, 
                 AVAILABLE_NOTES_DURATIONS.iter()
@@ -63,5 +67,59 @@ impl std::fmt::Display for Note
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "Note: height = [octave = {}, number = {}], duration = {}", self.height.octave, self.height.number, self.duration)
         
+    }
+}
+
+#[cfg(test)]
+mod notes_tests 
+{
+    #[test]
+    fn test_octave_change() 
+    {
+        let height = crate::notes::NoteHeight::new(1, 1);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 1);
+        let height = crate::notes::NoteHeight::new(1, 2);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 2);
+        let height = crate::notes::NoteHeight::new(1, 3);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 3);
+        let height = crate::notes::NoteHeight::new(1, 4);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 4);
+        let height = crate::notes::NoteHeight::new(1, 5);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 5);
+        let height = crate::notes::NoteHeight::new(1, 6);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 6);
+        let height = crate::notes::NoteHeight::new(1, 7);
+        assert_eq!(height.octave, 1);
+        assert_eq!(height.number, 7);
+        let height = crate::notes::NoteHeight::new(1, 8);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 1);
+        let height = crate::notes::NoteHeight::new(1, 9);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 2);
+        let height = crate::notes::NoteHeight::new(1, 10);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 3);
+        let height = crate::notes::NoteHeight::new(1, 11);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 4);
+        let height = crate::notes::NoteHeight::new(1, 12);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 5);
+        let height = crate::notes::NoteHeight::new(1, 13);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 6);
+        let height = crate::notes::NoteHeight::new(1, 14);
+        assert_eq!(height.octave, 2);
+        assert_eq!(height.number, 7);
+        let height = crate::notes::NoteHeight::new(1, 15);
+        assert_eq!(height.octave, 3);
+        assert_eq!(height.number, 1);   
     }
 }
